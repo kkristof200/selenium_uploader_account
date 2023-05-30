5f57fe36aa588fb72caaca1da33accd7dd0a8628
kkristof200 <kovacskristof200@gmail.com>
2023-05-30 13:55:47 +0300

    Update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="selenium_uploader_account",
-    version="0.2.2",
+    version="0.2.3",
     author="Kristof",
     description="selenium_uploader_account",
     long_description=long_description,
@@ -17,7 +17,7 @@ setuptools.setup(
     url="https://github.com/kkristof200/selenium_uploader_account",
     packages=setuptools.find_packages(),
     install_requires=[
-        'selenium-account>=0.2.2',
+        'selenium-account>=0.2.3',
         'setuptools>=67.8.0'
     ],
     classifiers=[
